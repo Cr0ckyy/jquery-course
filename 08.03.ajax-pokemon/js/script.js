852ad922b5091e26edb558404b62d23c948ee739
LI SHUFANG <lishufang1121@gmail.com>
2021-09-02 22:01:57 +0800

    Updated

--- 08.03.ajax-pokemon/js/script.js
+++ 08.03.ajax-pokemon/js/script.js
@@ -1,28 +1,36 @@
 $(function () {
 
-  // As another example of AJAX calls in jQuery, we access the PokéAPI to build
-  // a Pokédex.
+    // As another example of AJAX calls in jQuery, we access the PokéAPI to build
+    // a Pokédex.
 
-  var pokeapi = "https://pokeapi.co/api/v2/generation/1";
+    const pokeapi = "https://pokeapi.co/api/v2/generation/1";
 
-  $.getJSON(pokeapi).done(function(data) {
-    console.log(data);  // Explore available data in the Developer Tools Console
-    $.each(data.pokemon_species, function(index, pokemon) {
-      var name = capitalize(pokemon.name);
-      var paragraph = $("<p>").html("Pokémon species no. " + (index+1) + " is " + name);
-      paragraph.appendTo("#pokedex");
+    // TODO: getting the JSON data from pokeapi
+    $.getJSON(pokeapi).done(function (data) {
+
+        console.log(data);  // Explore available data in the Developer Tools Console
+
+        //  use the $.each method to obtain relevant Pokemon data from the API
+        $.each(data.pokemon_species, function (index, pokemon) {
+            const name = toString(pokemon.name);
+            const paragraph = $("<p>").html("Pokémon species no. " + (index + 1) + " is " + name);
+
+            // add the paragraph to pokedex div
+            paragraph.appendTo("#pokedex");
+        });
+
+    }).fail(function () {
+        // Handle error case
+        console.log("Call to PokéAPI failed.");
+
+    }).always(function () {
+        // Will always be executed (in either case).
+        console.log("Pokémon is awesome.")
     });
-  }).fail(function() {
-    // Handle error case
-    console.log("Call to PokéAPI failed.");
-  }).always(function() {
-    // Will always be executed (in either case).
-    console.log("Pokémon is awesome.")
-  });
 
 });
 
 // Capitalizes a given string.
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+function toString(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
--- 08.04.ajax-pokemon-2/js/script.js
+++ 08.04.ajax-pokemon-2/js/script.js
@@ -1,61 +1,63 @@
 $(function () {
 
-  // Let's further improve the Pokédex while diving deeper into AJAX with jQuery.
-  var pokeapi = "https://pokeapi.co/api/v2/generation/1";
-  var pokemonByName = "https://pokeapi.co/api/v2/pokemon/";
+    // Let's further improve the Pokédex while diving deeper into AJAX with jQuery.
+    const pokeapi = "https://pokeapi.co/api/v2/generation/1";
+    const pokemonByName = "https://pokeapi.co/api/v2/pokemon/";
 
-  $.getJSON(pokeapi).done(function(data) {
-    console.log(data);  // Explore available data in the Developer Tools Console
-    $.each(data.pokemon_species, function(index, pokemon) {
-      var name = capitalize(pokemon.name);
+    $.getJSON(pokeapi).done(function (data) {
+        console.log(data);  // Explore available data in the Developer Tools Console
+        $.each(data.pokemon_species, function (index, pokemon) {
+            const name = capitalize(pokemon.name);
 
-      // Add an additional link to show image of the Pokémon.
-      var boldName = $("<strong>").text(name);
-      var link = $("<a>")
-          .attr("id", pokemon.name)
-          .attr("href", "#")
-          .append(boldName);
+            // Add an additional link to show image of the Pokémon.
+            const boldName = $("<strong>").text(name);
 
-      var paragraph = $("<p>")
-          .html("Pokémon species no. " + (index+1) + " is ")
-          .append(link);
-      paragraph.appendTo("#pokedex");
+            const link = $("<a>")
+                .attr("id", pokemon.name)
+                .attr("href", "#")
+                .append(boldName);
 
-      // Add handler to the link to fetch and display image.
-      // The helper function showPokemon is defined below.
-      link.click(function() {
-        showPokemon(pokemon.name);  // API needs the non-capitalized name
-      });
+            const paragraph = $("<p>")
+                .html("Pokémon species no. " + (index + 1) + " is ")
+                .append(link);
+
+            paragraph.appendTo("#pokedex");
+
+            // Add handler to the link to fetch and display image.
+            // The helper function showPokemon is defined below.
+            link.click(function () {
+                showPokemon(pokemon.name);  // API needs the non-capitalized name
+            });
+        });
+    }).fail(function () {
+        // Handle error case
+        console.log("Call to PokéAPI failed.");
+    }).always(function () {
+        // Will always be executed (in either case).
+        console.log("Pokémon is awesome.")
     });
-  }).fail(function() {
-    // Handle error case
-    console.log("Call to PokéAPI failed.");
-  }).always(function() {
-    // Will always be executed (in either case).
-    console.log("Pokémon is awesome.")
-  });
 
-  // Only select this element once and store in variable.
-  var detailsDiv = $("#pokemon-details");
+    // Only select this element once and store in variable.
+    var detailsDiv = $("#pokemon-details");
 
-  function showPokemon(name) {
-    $.getJSON(pokemonByName + name).done(function(details) {
-      console.log(details);  // To explore data in browser
+    function showPokemon(name) {
+        $.getJSON(pokemonByName + name).done(function (details) {
+            console.log(details);  // To explore data in browser
 
-      // Show selected Pokémon in the details div.
-      var image = $("<img>").attr("src", details.sprites.front_default);
-      detailsDiv.empty()
-          .append("<h2>" + capitalize(name) + "</h2>")
-          .append(image);
+            // Show selected Pokémon in the details div.
+            const image = $("<img>").attr("src", details.sprites.front_default);
+            detailsDiv.empty()
+                .append("<h2>" + capitalize(name) + "</h2>")
+                .append(image);
 
-    }).fail(function(error) {
-      console.log("Could not retrieve details for " + name);
-    });
-  }
+        }).fail(function (error) {
+            console.log("Could not retrieve details for " + name);
+        });
+    }
 
 });
 
 // Capitalizes a given string.
 function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
